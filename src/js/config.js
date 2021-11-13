b46fd5153205c9b43b3bbc215f6dcc4de5a4b03e
Mainul Islam <m.main2402@gmail.com>
2021-11-13 18:12:58 +0600

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 export const TIMEOUT_SECOND = 10;
 export const IP_URL = 'https://api.ipify.org?format=json';
-export const API_URL = 'https://geo.ipify.org/api/v1?apiKey=${process.env.API_KEY}';
+export const API_URL = `https://geo.ipify.org/api/v1?apiKey=${process.env.API_KEY}`;
